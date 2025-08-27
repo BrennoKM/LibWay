d6aa3e36ac4d1e3cf0f25fbda801d108157d5b38
Brenno Kevyn <brennokm@gmail.com>
2025-08-27 20:23:11 -0300

    feat: Estrutura inicial do projeto com Spring Boot

---
+++ src/main/java/com/spring/libway/LibwayApplication.java
@@ -0,0 +1,13 @@
+package com.spring.libway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibwayApplication.class, args);
+	}
+
+}
