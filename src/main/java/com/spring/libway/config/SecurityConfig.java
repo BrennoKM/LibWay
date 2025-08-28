c0c7a2ddc66e6481a2499a8876d488b84174b676
Brenno Kevyn <brennokm@gmail.com>
2025-08-28 10:34:48 -0300

    fix(security): Corrige loop de redirecionamento permitindo acesso aos JSPs

--- src/main/java/com/spring/libway/config/SecurityConfig.java
+++ src/main/java/com/spring/libway/config/SecurityConfig.java
@@ -20,8 +20,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/login", "/registrar", "/css/**", "/js/**").permitAll()
+                        .requestMatchers("/login", "/registrar", "/WEB-INF/jsp/**", "/css/**", "/js/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
